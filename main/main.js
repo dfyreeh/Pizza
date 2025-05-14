2674f14f6297acdb851a18dc3306f479d771d7b4
unknown <mihalcann@gmail.com>
2025-05-14 17:10:40 +0300

    Version 5

--- main/main.js
+++ main/main.js
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 });
- 
 
 const btn = document.getElementById("openSecondModalBtn");
 const offcanvasEl = document.querySelector(".offcanvas");
@@ -86,16 +85,32 @@ const containerCardDesserts = document.getElementById("containerCardDesserts");
 const containerCardSauces = document.getElementById("containerCardSauces");
 const containerCardCoffees = document.getElementById("containerCardCoffees");
 const containerCardDrinks = document.getElementById("containerCardDrinks");
+const sliderCard = document.getElementById("sliderTrack");
+ 
+
 
 fetch("https://681b789317018fe5057bb776.mockapi.io/Pizza")
   .then((response) => response.json())
   .then((data) => {
+    const slider = data[0].slider;
     const pizzas = data[0].pizzas;
     const snacks = data[0].snacks;
     const desserts = data[0].desserts;
     const sauces = data[0].sauces;
     const coffees = data[0].coffees;
     const drinks = data[0].drinks;
+
+    slider.forEach((slide) => {
+        sliderCard.innerHTML += `
+          <div class="card-slider-item">
+            <img
+              src="${slide.img}"
+              class="img-fluid rounded-3"
+              alt="1"
+            />
+          </div>
+        `;
+      });
 
     pizzas.forEach((pizza, index) => {
       const modalId = `modal-${index}`;
